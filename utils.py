a262fcb25df8958f3f4fdbb4f060904c5b26e229
DaoChaShao <27882272@qq.com>
2025-09-17 19:18:11 +0800

    fix: fix an options list type

--- utils.py
+++ utils.py
@@ -3,3 +3,7 @@ from random import choice
 def greet():
     options: list[str] = ["Hello, World", "你好，世界"]
     return choice(options)
+
+def temperature_getter():
+    options: list[int] = [36, 37, 39]
+    return choice(options)
