a5c1c0287095b8f471594b126ea9ce52d6c654a3
DaoChaShao <27882272@qq.com>
2025-09-18 13:29:01 +0800

    feat: add an enum with yes and no

---
+++ +helper/YesNo.m
@@ -0,0 +1,15 @@
+% MATLAB Ver. : 25.1.0.2973910 (R2025a) Update 1
+% Style Guide : MATLAB Style Guidelines 1.6
+% Encoding    : ***** UTF-8 *****
+% File        : yesNo.m
+% Author      : daochashao
+% Date        : 2025-09-18 12:43:15
+% Code Ver.   : 0.1.0
+% Desc        : 
+
+classdef YesNo
+    enumeration
+        Yes
+        No
+    end
+end
