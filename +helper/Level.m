7e86de07aae05efe317cd6e4cd669d64b19c2d08
DaoChaShao <27882272@qq.com>
2025-09-18 13:27:34 +0800

    feat: add an enum with different level

---
+++ +helper/Level.m
@@ -0,0 +1,16 @@
+% MATLAB Ver. : 25.1.0.2973910 (R2025a) Update 1
+% Style Guide : MATLAB Style Guidelines 1.6
+% Encoding    : ***** UTF-8 *****
+% File        : level.m
+% Author      : daochashao
+% Date        : 2025-09-18 12:48:41
+% Code Ver.   : 0.1.0
+% Desc        : 
+
+classdef Level
+    enumeration
+        Low
+        Medium
+        High
+    end
+end
