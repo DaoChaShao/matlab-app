86be1dbd42bd89eb3668f2d9fa36213175c3e013
DaoChaShao <27882272@qq.com>
2025-09-18 13:28:21 +0800

    feat: add an enum with different light colours

---
+++ +helper/Lights.m
@@ -0,0 +1,21 @@
+% MATLAB Ver. : 25.1.0.2973910 (R2025a) Update 1
+% Style Guide : MATLAB Style Guidelines 1.6
+% Encoding    : ***** UTF-8 *****
+% File        : Lights.m
+% Author      : daochashao
+% Date        : 2025-09-18 13:02:52
+% Code Ver.   : 0.1.0
+% Desc        : 
+
+classdef Lights
+    enumeration
+        Red
+        Yellow
+        Green
+        Blue
+        Cyan
+        Magenta
+        Black
+        White
+    end
+end
